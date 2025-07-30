7c71ff4c605f2ab1b9b13ad3f6784628d87e6dda
Vladimir Volkov <volkovvova67@gmail.com>
2025-07-30 11:54:37 +0400

    refactor: leaderboard page e2e tests

--- cypress/e2e/leaderboardPage.cy.ts
+++ cypress/e2e/leaderboardPage.cy.ts
@@ -1,19 +1,6 @@
 import { ROUTES_PATHS } from "../../src/05_shared/config"
-
-enum PlayerNames {
-  Player1 = "Player1",
-  Player2 = "Player2",
-}
-
-const createWinnerClicksTable = (cellWidth: number, cellHeight: number) => {
-  return [
-    { x: cellWidth * 0.5, y: cellHeight * 0.5 }, // 1,1 - row,col
-    { x: cellWidth * 0.5, y: cellHeight * 1.5 }, // 2,1
-    { x: cellWidth * 1.5, y: cellHeight * 0.5 }, // 1,2
-    { x: cellWidth * 1.5, y: cellHeight * 0.5 }, // 2,2
-    { x: cellWidth * 2.5, y: cellHeight * 0.5 }, // 1,3
-  ]
-}
+import { PlayersNames, Selectors } from "../fixtures/constants"
+import { getOpponentName } from "../fixtures/game"
 
 describe("Leaderboard page", () => {
   beforeEach(() => {
@@ -21,68 +8,48 @@ describe("Leaderboard page", () => {
     cy.clearLocalStorage()
 
     // Registration players
-    cy.get('[data-cy="first-player-name"]').type(PlayerNames.Player1)
-    cy.get('[data-cy="second-player-name"').type(PlayerNames.Player2)
-    cy.get('[data-cy="button-cross-symbol"').click()
-    cy.get('[data-cy="play-button"').click()
-
-    cy.get('[data-cy="leaderboard-link"]').click()
+    cy.get(Selectors.FIRST_PLAYER_NAME).type(PlayersNames.Player1)
+    cy.get(Selectors.SECOND_PLAYER_NAME).type(PlayersNames.Player2)
+    cy.get(Selectors.CROSS_SYMBOL_BUTTON).click()
+    cy.get(Selectors.PLAY_BUTTON).click()
+    cy.get(Selectors.LEADERBOARD_LINK).click()
   })
 
   it("should page load", () => {
-    // Check page load
-    cy.get('[data-cy="leaderboard-page"]').should("be.visible")
+    cy.get(Selectors.LEADERBOARD_PAGE).should("be.visible")
 
-    // Get leaderboard items and check player names and records
-    cy.get('[data-cy="leaderboard-list-item"]')
+    cy.get(Selectors.LEADERBOARD_ITEM)
       .should("have.length", 2)
       .each(($el, index) => {
         const playerName =
-          index === 0 ? PlayerNames.Player1 : PlayerNames.Player2
-
+          index === 0 ? PlayersNames.Player1 : PlayersNames.Player2
         cy.wrap($el).should("contain", playerName).and("contain", "0")
       })
   })
 
   it("should increment winner's record in leaderboard", () => {
-    // Start game
-    cy.get('[data-cy="back-play-link"]').click()
+    cy.get(Selectors.BACK_PLAY_LINK).click()
 
-    // Get current player name - he winner
     let winnerName: string
-    cy.get('[data-cy="current-player-name"]').then(($nameElement) => {
+    let oponnentName: string
+
+    cy.get(Selectors.CURRENT_PLAYER_NAME).then(($nameElement) => {
       winnerName = $nameElement.text().trim()
+      oponnentName = getOpponentName(winnerName)
     })
 
-    // Set winner clicks
-    cy.get('[data-cy="game-canvas"]').then(($canvas) => {
-      const clicks = createWinnerClicksTable(
-        $canvas.width()! / 3,
-        $canvas.height()! / 3,
-      )
+    cy.makeWinningMoves()
 
-      clicks.forEach((click) => {
-        cy.wrap($canvas).click(click.x, click.y).wait(100)
-      })
-    })
+    cy.get(Selectors.PLAY_AGAIN_BUTTON).click()
+    cy.get(Selectors.LEADERBOARD_LINK).click()
 
-    // Click play again and go leaderboard
-    cy.get('[data-cy="play-again-button"]').click()
-    cy.get('[data-cy="leaderboard-link"]').click()
-
-    // Check leaderboard table
-    cy.get('[data-cy="leaderboard-list-item"]')
+    cy.get(Selectors.LEADERBOARD_ITEM)
       .should("have.length", 2)
       .each(($el, index) => {
-        if (index === 0) {
-          // Top 1 a have 1 win
-          cy.wrap($el).should("contain", winnerName).and("contain", "1")
-        } else {
-          // Top 2 a have 0 win
-          const otherPlayerName =
-            winnerName === "Player1" ? "Player2" : "Player1"
-          cy.wrap($el).should("contain", otherPlayerName).and("contain", "0")
-        }
+        const playerName = index === 0 ? winnerName : oponnentName
+        const record = index === 0 ? "1" : "0"
+
+        cy.wrap($el).should("contain", playerName).and("contain", record)
       })
   })
 })
---
+++ cypress/fixtures/constants.ts
@@ -0,0 +1,20 @@
+export const PlayersNames = { Player1: "Player1", Player2: "Player2" } as const
+
+export const Selectors = {
+  // Registration Page
+  FIRST_PLAYER_NAME: '[data-cy="first-player-name"]',
+  SECOND_PLAYER_NAME: '[data-cy="second-player-name"]',
+  CROSS_SYMBOL_BUTTON: '[data-cy="button-cross-symbol"]',
+  PLAY_BUTTON: '[data-cy="play-button"]',
+
+  // Game Page
+  GAME_CANVAS: '[data-cy="game-canvas"]',
+  CURRENT_PLAYER_NAME: '[data-cy="current-player-name"]',
+  PLAY_AGAIN_BUTTON: '[data-cy="play-again-button"]',
+  BACK_PLAY_LINK: '[data-cy="back-play-link"]',
+  LEADERBOARD_LINK: '[data-cy="leaderboard-link"]',
+
+  // Leaderboard Page
+  LEADERBOARD_PAGE: '[data-cy="leaderboard-page"]',
+  LEADERBOARD_ITEM: '[data-cy="leaderboard-list-item"]',
+} as const
